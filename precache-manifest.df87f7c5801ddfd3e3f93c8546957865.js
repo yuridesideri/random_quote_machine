f07a3eef0c7e846d96e4ba758f6cda82316a3c78
Yuri <yuridesideri12@gmail.com>
2022-09-25 16:21:48 -0300

    Updates

---
+++ precache-manifest.df87f7c5801ddfd3e3f93c8546957865.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbc650ddf784b8656f3e",
+    "url": "/static/js/main.bbc650dd.chunk.js"
+  },
+  {
+    "revision": "29f346a8dcfcd967574f",
+    "url": "/static/js/1.29f346a8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9347967db3b064255c4075dfcdb7ab8e",
+    "url": "/index.html"
+  }
+];
