b0e006968eb7909c20ccbd6994aa7cf8103e8f74
Yuri <yuridesideri12@gmail.com>
2022-09-25 13:10:19 -0300

    twitter and tumblr functionaties implemented

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -98,19 +98,30 @@ export default function Wrapper(props) {
         setQuote(APIdata[Math.floor(APIdata.length * Math.random())]);
     }
 
+    function tweeterIntentGenerator(str){
+        const stringWebIntent = "https://twitter.com/intent/tweet?hashtags=quotes&text=" + str.text + " -" + (str.author === null? "Unknown" : str.author);
+        return stringWebIntent;
+
+    }
+    function tumblrIntentGenerator(str){
+        if (str !== undefined){
+            const stringWebIntent = `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=${(str.author === null? "Unknown" : str.author)}&content=${str.text}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`
+            return stringWebIntent;
+        }
+    }
 
     return(
             <Wrapperstyle id="quote-box">
                 <div className='quote-wrapper'>
-                    <p id='text'>{quote.text}</p>
+                    <p id='text'>{'"' + quote.text + '"'}</p>
                     <div>
                         <p id='author'>-{quote.author === null? 'Unknown' : quote.author}</p>
                     </div>
                 </div>
                 <div className='interactions-wrapper'>
                     <div>
-                    <a rel="noreferrer" target="_blank" href="https://twitter.com/intent/tweet" id="tweet-quote"><ImTwitter /></a>
-                    <a href="" id="tumblr-quote"><ImTumblr /></a>
+                    <a rel="noopener noreferrer" target="_blank" href={tweeterIntentGenerator(quote)} id="tweet-quote"><ImTwitter /></a>
+                    <a rel="noopener noreferrer" target="_blank" href={tumblrIntentGenerator(quote)} id="tumblr-quote"><ImTumblr /></a>
                     </div>
                     <button onClick={updateQuote} id='new-quote'>New quote</button>
                 </div>
